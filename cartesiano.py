ca0703873ff6e51c3c57295730b543cf26412bdc
Diego Sanabria <dsanabriagomez@outlook.es>
2023-02-14 13:42:31 -0500

    Initial Commit

--- cartesiano.py
+++ cartesiano.py
@@ -1,5 +1,10 @@
 class Punto:
     def __init__(self, punto_x, punto_y):
-        self.punto_x = punto_x
-        self.punto_y = punto_y
+        self.x: float = punto_x
+        self.y: float = punto_y
 
+    def mostrar_Puntos(self):
+        print((self.x, self.y))
+
+point = Punto(7,8)
+point.mostrar_Puntos()
