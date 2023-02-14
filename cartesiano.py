156a332753e396e158ae4e2b074a9c040acdb6a3
Diego Sanabria <dsanabriagomez@outlook.es>
2023-02-14 13:11:48 -0500

    Initial Commit

--- cartesiano.py
+++ cartesiano.py
@@ -2,3 +2,4 @@ class Punto:
     def __init__(self, punto_x, punto_y):
         self.punto_x = punto_x
         self.punto_y = punto_y
+
--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 class Vehículo:
     def __init__(self, kilometraje, velocidad_maxima):
+        self.kilometraje = kilometraje
+        self.velocidad_maxima = velocidad_maxima
